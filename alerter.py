320c19b55c8a598c3a721c512c1aa61a5489155a
Suneel Varma <49975815+Suneelvarma5fi@users.noreply.github.com>
2022-11-30 08:01:40 +0530

    Added an assertion to test & altered stub with threshold

--- alerter.py
+++ alerter.py
@@ -1,11 +1,13 @@
 alert_failure_count = 0
+temperature_threshold = 100
 
 def network_alert_stub(celcius):
+    global temperature_threshold
     print(f'ALERT: Temperature is {celcius} celcius')
     # Return 200 for ok
     # Return 500 for not-ok
     # stub always succeeds and returns 200
-    return 200
+    return 200 if temperature_threshold < 100 else 500
 
 def alert_in_celcius(farenheit):
     celcius = (farenheit - 32) * 5 / 9
@@ -21,5 +23,6 @@ def alert_in_celcius(farenheit):
 
 alert_in_celcius(400.5)
 alert_in_celcius(303.6)
+assert(alert_failure_count == 0)
 print(f'{alert_failure_count} alerts failed.')
 print('All is well (maybe!)')
