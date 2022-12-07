c0882eccfda41ee88a50cbefbb10f770897ae026
Suneel Varma <49975815+Suneelvarma5fi@users.noreply.github.com>
2022-12-07 11:38:20 +0530

    Segregated test cases from main code

--- tshirts.py
+++ tshirts.py
@@ -2,14 +2,7 @@
 def size(cms):
     if cms < 38:
         return 'S'
-    elif cms > 38 and cms < 42:
+    elif cms >= 38 and cms < 42:
         return 'M'
     else:
         return 'L'
-
-
-assert(size(37) == 'S')
-assert(size(38) == 'S') #code fails to categorize properly when it tshirt size is exactly 38, it classifies as Large but it is not.
-assert(size(40) == 'M')
-assert(size(43) == 'L')
-print("All is well (maybe!)\n")
